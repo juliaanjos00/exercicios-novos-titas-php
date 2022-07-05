3bbf762c05a85c9b4d2ad8b3a0c0f83ff0086077
juliaanjos0 <anjosjulianjos09@gmail.com>
2022-07-05 19:31:58 -0300

    corrigi a identaçao do codigo,mudei o name do select,adicionei o value no option do exercicio 6.

--- Exercicio-6/index.php
+++ Exercicio-6/index.php
@@ -8,30 +8,28 @@
     <title>Exercicio-6</title>
 </head>
 <body>
-    <div class="form">
-        <h2>Exercício 6</h2>
+        <div class="form">
+            <h2>Exercício 6</h2>
 
-        <br />
-        <h3> 
-            Entre com um número inteiro entre 1 e 12 e o algoritimo vai mostrar um mês correspondente ao número
-        </h3>
-        <br />
+            <br /><h3> 
+                Entre com um número inteiro entre 1 e 12 e o algoritimo vai mostrar um mês correspondente ao número
+            </h3><br />
 
-        <form id="formulario" action="/Exercicio-6/index.php" method="post">
-            <div class="input-field">
-                <input type="number" name="value1" autocomplete="off" placeholder="Adicione um numero!"/>
-                <div class="underline"></div>
-            </div>
-              <br /><select name="box" action="#"> 
-                  
-              <option>Português(Brasil)</option> 
-              <option> English </option>
-            
-             </select><br />
-               <br /> <input type="submit" class="button" name="enviar" value="Enviar" class="enviar"/>
+            <form id="formulario" action="/Exercicio-6/index.php" method="post">
+                <div class="input-field">
+                    <input type="number" name="value1" autocomplete="off" placeholder="Adicione um numero!"/>
+                    <div class="underline"></div>
+                </div>
 
-     </form> 
-    </div> 
+                <br /><select name="lingua" action="#"> 
+                    <option value='pt-br'>Português(Brasil)</option> 
+                    <option value='en'> English </option>
+                </select><br />
+
+                <br /> <input type="submit" class="button" name="enviar" value="Enviar" class="enviar"/>
+
+            </form> 
+        </div> 
         
 </body>
 </html>
