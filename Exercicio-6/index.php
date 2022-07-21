a4493f9e12e564e837bb568b9b748c25886a285a
juliaanjos0 <anjosjulianjos09@gmail.com>
2022-07-21 01:16:01 -0300

    feita a identaçao do exercicio 6

--- Exercicio-6/index.php
+++ Exercicio-6/index.php
@@ -18,43 +18,40 @@
         <br />
 
         <form id="formulario" action="/Exercicio-6/index.php" method="post">
-            <div class="input-field">
+               <div class="input-field">
                 <input type="number" name="value1" autocomplete="off" placeholder="Adicione um numero!"/>
                 <div class="underline"></div>
-            </div>
-              <br /><select name="lingua" action="#"> 
+                </div>
+                <br /><select name="lingua" action="#"> 
                   
-              <option value='pt-br'> Português(Brasil)</option> 
-              <option value='en'> English </option>
-            
-             </select><br />
-               <br /> <input type="submit" class="button" name="enviar" value="Enviar" class="enviar"/>
+                 <option value='pt-br'> Português(Brasil)</option> 
+                 <option value='en'> English </option>
+                
+                    </select><br />
+                 <br /> <input type="submit" class="button" name="enviar" value="Enviar" class="enviar"/>
      
       
-    <?php
+            <?php
 
-    $mesEn=['January','February','March','April','May','June','July','Agust','September','October','November','December'];
-    $mesPtbr=['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'];
-     
-    if(isset($_POST['value1'])){
-        $mes= $_POST['value1'];
+                $mesEn=['January','February','March','April','May','June','July','Agust','September','October','November','December'];
+                $mesPtbr=['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'];
+                
+                if(isset($_POST['value1'])){
+                    $mes= $_POST['value1'];
 
-        $language=$_POST['lingua'];
-     if($language=="pt-br"){
-         echo   $mesPtbr[$mes -1];
-   
-     } else { 
-        echo $mesEn[$mes -1];
-     }
-     }
-     
-
-    
-  
-    ?>
+                    $language=$_POST['lingua'];
+                if($language=="pt-br"){
+                    echo   $mesPtbr[$mes -1];
+            
+                } else { 
+                    echo $mesEn[$mes -1];
+                }
+                }
+        
+            ?>
 
 
-     </form> 
+        </form> 
     </div> 
         
 </body>
